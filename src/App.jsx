f886a821ac8ca5b887ec5bc9af24a6c9653f162a
danalmeidadev <devdanalmeida@gmail.com>
2020-06-22 12:12:36 -0300

    Projeto Calculadora

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './GlobalCss/styles.css';
+import Calculadora from './components/Calculadora';
+
+function App() {
+  return (
+    <>
+    <h1>Calculadora</h1>
+    <Calculadora />    
+    </>
+  );
+ 
+}
+
+export default App;
